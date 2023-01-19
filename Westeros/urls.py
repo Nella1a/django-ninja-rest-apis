8af54a377113f8880ab34c5b1e43e8e25b9dc1e9
MarthaN <m.allen@gmx.at>
2023-01-19 23:03:12 +0100

    Create first ninja api instance,add api endpoint,add api path to urlspattern

--- Westeros/urls.py
+++ Westeros/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ninja import NinjaAPI
+from lannister.api import router as lannister_router
+
+api = NinjaAPI()
+api.add_router("/lannister/", lannister_router)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
---
+++ lannister/api.py
@@ -0,0 +1,8 @@
+from ninja import Router
+
+router = Router()
+
+# define an api-path-endpoint
+@router.get("/home")
+def home(request):
+    return "A Lannster always pays their debts"
